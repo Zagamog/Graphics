4b3ff21f5e604bd91ddb4eec2f014201142c4d3c
Zagamog <wb164718@PN4411986.wb.ad.worldbank.org>
2016-07-31 13:30:19 -0400

    Second

--- L1_1_Script.R
+++ L1_1_Script.R
@@ -4,3 +4,15 @@ library(ggplot2)
 # Change the command below so that cyl is treated as factor
 ggplot(mtcars, aes(x = factor(cyl), y = mpg)) +
   geom_point()
+
+# A scatter plot has been made for you
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point()
+
+# Replace ___ with the correct vector
+ggplot(mtcars, aes(x = wt, y = mpg, col = disp)) +
+  geom_point()
+
+# Replace ___ with the correct vector
+ggplot(mtcars, aes(x = wt, y = mpg, size = disp)) +
+  geom_point()
