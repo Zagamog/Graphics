f31908d98d6719fd0b142dc824c29c8518a00d68
Zagamog <wb164718@PN4411986.wb.ad.worldbank.org>
2016-08-01 18:23:58 -0400

    Fifth

--- L1_1_Script.R
+++ L1_1_Script.R
@@ -238,6 +238,106 @@ ggplot(mtcars, aes(x = wt, y = mpg, fill = cyl)) +
 ggplot(mtcars, aes(x = wt, y = mpg, fill = cyl)) +
   geom_text(label = "x", color = 'red', size = 10)
 
+####
+
+# Map mpg onto x, qsec onto y and factor(cyl) onto col
+ggplot(mtcars,aes(x=mpg,y=qsec,col=factor(cyl))) +
+  geom_point()
+
+# Add mapping: factor(am) onto shape
+ggplot(mtcars,aes(x=mpg,y=qsec,col=factor(cyl),shape=factor(am))) +
+  geom_point()
+
+# Add mapping: (hp/wt) onto size
+ggplot(mtcars,aes(x=mpg,y=qsec,col=factor(cyl),shape=factor(am),size=(hp/wt))) +
+  geom_point()
+
+
+
+#######################################
+
+cyl.am <- ggplot(mtcars, aes(x = factor(cyl), fill = factor(am))) +
+           
+# The base layer, cyl.am, is available for you
+# Add geom (position = "stack" by default)
+cyl.am + 
+  geom_bar()
+
+# Fill - show proportion
+cyl.am + 
+  geom_bar(position = "fill")  
+
+# Dodging - principles of similarity and proximity
+cyl.am +
+  geom_bar(position = "dodge") 
+
+# Clean up the axes with scale_ functions
+val = c("#E41A1C", "#377EB8")
+lab = c("Manual", "Automatic")
+cyl.am +
+  geom_bar(position = "dodge") +
+  scale_x_discrete("Cylinders") + 
+  scale_y_continuous("Number") +
+  scale_fill_manual("Transmission", 
+                    values = val,
+                    labels = lab) 
+##############################################
+
+# Add a new column called group
+mtcars$group <- 0
+
+# Create jittered plot of mtcars: mpg onto x, group onto y
+ggplot(mtcars, aes(x = mpg, y = group)) + geom_jitter()
+
+
+# Change the y aesthetic limits
+ggplot(mtcars, aes(x = mpg, y = group)) +
+  geom_jitter() +
+  scale_y_continuous(limits=c(-2,2))
+
+# Bonus exercise from Video
+posn.jitter <- position_jitter(width=.5)
+ggplot(mtcars, aes(x = mpg, y = group)) +
+  geom_point(position=posn.jitter, color="red",size=2) +
+  scale_y_continuous(limits=c(-3,3))
+###############################################
+
+# Basic scatter plot: wt on x-axis and mpg on y-axis; map cyl to col
+ggplot(mtcars, aes(x=wt,y=mpg, col=cyl))+ 
+  geom_point(size=4)
+
+
+# Hollow circles - an improvement
+ggplot(mtcars, aes(x=wt,y=mpg, col=cyl))+ 
+  geom_point(size=4,shape=1)
+
+
+# Add transparency - very nice
+ggplot(mtcars, aes(x=wt,y=mpg, col=cyl))+ 
+  geom_point(size=4,shape=1, alpha=0.6)
+
+#################################################
+
+# Scatter plot: carat (x), price (y), clarity (col)
+ggplot(diamonds, aes(x=carat, y=price, col=clarity)) +
+         geom_point() 
+       
+# Adjust for overplotting
+ggplot(diamonds,aes(x=carat, y=price, col=clarity)) +
+                geom_point(alpha=0.5) 
+              
+# Scatter plot: clarity (x), carat (y), price (col)
+ggplot(diamonds,aes(x=clarity, y=carat, col=price)) +
+               geom_point(alpha=0.5) 
+             
+                     
+# Dot plot with jittering
+ggplot(diamonds,aes(x=clarity, y=carat, col=price)) +
+            geom_point(alpha=0.5,position="jitter")
+                            
+                            
+                            
+
 
 
 
